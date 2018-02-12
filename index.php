2d2a99a002c1310c8178b5336f3952b4f07777e7
syziddy65 <cyriluket12@gmail.com>
2018-02-12 23:27:03 +0100

    i jst encrypted

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$offset = 0;
+$string =strtolower($_POST['string']);
+$alpha = range('a', 'z');
+$split = str_split($string);
+$split_length = count($split);
+$alpha_length = count($alpha);
+for ($i=0; $i < $split_length ; $i++) { 
+	$find = $split[$i];
+	$pos = array_search($find, $alpha ); 
+	$index = $pos + 1;
+	$mod = $index%2;
+	if ($mod > 0) {
+		$x = ($index*3) + 1;
+		echo "o".$x;
+	}else{
+		
+		$x = $index/2;
+		echo "e".$x;
+	}
+
+	
+}
+
+ ?>
+<form action="" method="POST">
+	<input type="" name="string">
+	<input type="submit" value="encrypt">
+</form>
