15c6c77faa252e87791e16d81bde0288a857fc67
syziddy65 <cyriluket12@gmail.com>
2018-02-12 23:46:52 +0100

    i jst encrypted

--- index.php
+++ index.php
@@ -5,25 +5,31 @@
 <?php
 $offset = 0;
 $string =strtolower($_POST['string']);
-$alpha = range('a', 'z');
-$split = str_split($string);
-$split_length = count($split);
-$alpha_length = count($alpha);
-for ($i=0; $i < $split_length ; $i++) { 
-	$find = $split[$i];
-	$pos = array_search($find, $alpha ); 
-	$index = $pos + 1;
-	$mod = $index%2;
-	if ($mod > 0) {
-		$x = ($index*3) + 1;
-		echo "o".$x;
-	}else{
+if (isset($string)&&! empty($string)) {
+	$alpha = range('a', 'z');
+	$split = str_split($string);
+	$split_length = count($split);
+	$alpha_length = count($alpha);
+	for ($i=0; $i < $split_length ; $i++) { 
+		$find = $split[$i];
+		$pos = array_search($find, $alpha ); 
+		$index = $pos + 1;
+		$mod = $index%2;
+		if ($mod > 0) {
+			$x = ($index*3) + 1;
+			echo "o".$x;
+		}else{
 		
-		$x = $index/2;
-		echo "e".$x;
-	}
+			$x = $index/2;
+			echo "e".$x;
+		}
 
 	
+	}
+
+}else {
+	echo "please type a string";
 }
+
 
  ?>
